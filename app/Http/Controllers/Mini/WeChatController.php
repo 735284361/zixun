28f1ea5db4ac650e5df33f267c3c642123691c93
十年饮冰 <735284361@qq.com>
2019-10-22 09:39:10 +0800

    注册登录加入unionid

--- app/Http/Controllers/Mini/WeChatController.php
+++ app/Http/Controllers/Mini/WeChatController.php
@@ -19,17 +19,14 @@ class WeChatController extends Controller
         $code = $request->code;
         $miniProgram = \EasyWeChat::miniProgram();
         //获取openid session_key
-//        $data = $miniProgram->auth->session($code);
-//        //判断code是否过期
-//        if (isset($data['errcode'])) {
-//            return response()->json(['code'=>1,'msg'=>'code已过期或不正确']);
-//        }
-        //获取小程序信息 当前只获取了个昵称及输入的工号密码
-//        $openId = $data['openid'];
-//        $sessionKey = $data['session_key'];
-
-        $openId = 'oR-sN5LZKPcvMG1_CkpoLCQTuFmo';
-        $sessionKey = 'S8cTQ+DxFFB6BT/UKvV6bA==';
+        $data = $miniProgram->auth->session($code);
+        //判断code是否过期
+        if (isset($data['errcode'])) {
+            return response()->json(['code'=>1,'msg'=>'code已过期或不正确']);
+        }
+//        获取小程序信息 当前只获取了个昵称及输入的工号密码
+        $openId = $data['openid'];
+        $sessionKey = $data['session_key'];
 
         // 判断子表是否存在该用户
         $userSub = UsersSub::where('open_id',$openId)->first();
@@ -56,15 +53,15 @@ class WeChatController extends Controller
 
     public function register(Request $request)
     {
-        $sessionKey = "oyLUVHs3xyPN8arT2JPIsg==";//$request->get('sessionKey');
-        $iv = "CxAkIemE+ff5RldTfjXrTA==";//$request->get('iv');
-        $encryptedData = "r8BGYpPIsBRxuE3hPG799s18C65v2DI5CiZRM6W0KvtKl9Oi3RKRn6XsatlzSeBiTScb04lbms6Tj9oApWofKj4YLTO5oe3OlCYZ2/oU6V7YF5cRHuXNpJJXrQ5f2jUC3OlCVKtcGHimK+fViaNxxWUppWVK06is6xcxgoFD0RL8AT6xDgnzY3iy2k3olqhx4PFBShl0E6mysabOuG3Huk9yxEU+HpEF5NCHt1FDvTSLzZ3Wq8uYYdeD8s+19Q4m+1VigfBMmrOorLRbp2wpwQXY4B1xWw8C88AJ+UXguM1cLSSkfxI+gy7kWwIhb+JHiRACFpn+hhFVUN3NM2IjrY3lCJh+CMPDrg0keFhHc00TaAj/Hfbznq7L7pLpTDdTeu/Nwa5O3vDEvUDe9DqkSqSFs7V6eIqENMeMzTkXi6bg+MakP37N6gQVmKKSmPmzyHNexiZiMZjLfyHyF5ZO5A==";
+        $code = $request->get('code');
+        $iv = $request->get('iv');
+        $encryptedData = $request->get('encryptedData');
         // 解压数据
         $app = \EasyWeChat::miniProgram(); // 小程序
+        $sessionData = $app->auth->session($code);
+        $sessionKey = $sessionData['session_key'];
+        $unionId = $sessionData['unionid'];
         $data = $app->encryptor->decryptData($sessionKey, $iv, $encryptedData);
-
-        $unionId = 'oXWAQv6dq-KX5ygK8VEqFhTv6go';
-
         // 查看主库是否存在该用户
         $time = time();
         $user = User::firstOrCreate(['unionid'=>$unionId],[
