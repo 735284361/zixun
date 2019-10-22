c42d7e899d7d0b4c7edaf5b78c0654f7c4dfbe28
十年饮冰 <735284361@qq.com>
2019-10-22 13:31:53 +0800

    api.php 冗余删除

--- routes/api.php
+++ routes/api.php
@@ -17,24 +17,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::any('/wechat/decryptData', 'Mini\WeChatController@decryptData');
-Route::any('/wechat/login', 'Mini\WeChatController@login');
-Route::any('/wechat/register', 'Mini\WeChatController@register');
-
-Route::group(['middleware' => ['web', 'wechat.oauth']], function () {
-    Route::get('/user', function () {
-        $user = session('wechat.oauth_user.default'); // 拿到授权用户资料
-
-        dd($user);
-    });
-});
+Route::get('/wechat/login', 'Mini\WeChatController@login');
+Route::get('/wechat/register', 'Mini\WeChatController@register');
 
 //前端小程序拿到的地址：https://域名/api/v1/自己写的接口
 Route::group(['prefix' => '/v1'], function () {
     Route::any('/user/login', 'UserController@wxLogin');
 });
-
-//Route::any('/wechat/profile', 'Mini\WeChatController@profile');
 
 Route::group(['middleware' => ['auth:api']], function () {
     Route::any('/wechat/profile', 'Mini\WeChatController@profile');
