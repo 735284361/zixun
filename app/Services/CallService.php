09037d54a5a6e929ee3c686b57db39c6f0fb245d
十年饮冰 <735284361@qq.com>
2019-11-13 15:31:36 +0800

    123

--- app/Services/CallService.php
+++ app/Services/CallService.php
@@ -182,7 +182,6 @@ class CallService
      */
     public function onCallEvent($jsonBody)
     {
-//        Log::info($jsonBody);
 //        $jsonArr = json_decode($jsonBody, true); //将通知消息解析为关联数组
         $jsonArr = $jsonBody;
 
